4125732c49287a2f227c99977b17e46e2dd20d55
wenzhexuan <3322926059@qq.com>
2024-09-21 15:01:56 +0800

    FOUND

--- found.py
+++ found.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import pygame as pyg
+pyg.init()
+screen=pyg.display.set_mode([640,480])
+screen.fill([255,255,255])
+running=True
+while running:
+    for event in pyg.event.get():
+        if event.type==pyg.QUIT:
+            running=False
+pyg.quit()
